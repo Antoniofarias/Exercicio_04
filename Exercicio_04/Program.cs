dbd79ce88248defceb6808d11dbfdf72d9493dec
Antonio <antoniofjj@hotmail.com>
2019-02-12 11:45:38 -0200

    Desenvolva um algoritmo que solicite a entrada da idade de um determinado usuário, se for menor que 18 anos exiba na cor vermelha “Sem permissão”, caso seja maior ou igual a 18 anos exiba na cor verde “Permissão concedida”.

--- Exercicio_04/Program.cs
+++ Exercicio_04/Program.cs
@@ -10,6 +10,28 @@ namespace Exercicio_04
     {
         static void Main(string[] args)
         {
+            //Desenvolva um algoritmo que solicite a entrada da idade de um determinado usuário, se for menor que 18 anos exiba na cor vermelha “Sem permissão”, caso seja maior ou igual a 18 anos exiba na cor verde “Permissão concedida”.
+
+            processar();
+            Console.ReadKey();
+        }
+
+        private static void processar()
+        {
+            int idade;
+            Console.Write("Digite sua idade ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade < 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sem permissão");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Permissão concedida");
+            }
         }
     }
 }
